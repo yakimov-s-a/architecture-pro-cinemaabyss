0d03744d3369aab7ad615a6e37ccb6e81fee6669
Sergey Yakimov <yakimov.s.a@yandex.ru>
2026-03-07 21:55:39 +0200

    Remove events service proxying

--- src/microservices/proxy/main.go
+++ src/microservices/proxy/main.go
@@ -11,88 +11,37 @@ import (
 	"strconv"
 )
 
-type Config struct {
-	Port                   string
-	MonolithUrl            *url.URL
-	GradualMigration       bool
-	MoviesServiceUrl       *url.URL
-	MoviesMigrationPercent int
-	EventsServiceUrl       *url.URL
-	EventsMigrationPercent int
-}
-
 func main() {
-	config, err := getConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	http.Handle("/", &httputil.ReverseProxy{
-		Rewrite: func(r *httputil.ProxyRequest) {
-			r.SetXForwarded()
-			r.SetURL(config.MonolithUrl)
-		},
-	})
-
-	if config.GradualMigration {
-		http.Handle("/api/movies", newWeightedReverseProxy(
-			config.MonolithUrl,
-			config.MoviesServiceUrl,
-			config.MoviesMigrationPercent,
-		))
-		http.Handle("/api/events", newWeightedReverseProxy(
-			config.MonolithUrl,
-			config.EventsServiceUrl,
-			config.EventsMigrationPercent,
-		))
-	}
-
-	http.HandleFunc("/health", handleHealth)
-	log.Fatal(http.ListenAndServe(":"+config.Port, nil))
-}
-
-func getConfig() (*Config, error) {
 	port := getEnv("PORT", "8000")
 
 	monolithUrl, err := url.Parse(getEnv("MONOLITH_URL", "http://monolith:8080"))
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
 
 	gradualMigration, err := strconv.ParseBool(getEnv("GRADUAL_MIGRATION", "true"))
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
 
 	moviesServiceUrl, err := url.Parse(getEnv("MOVIES_SERVICE_URL", "http://movies-service:8081"))
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
 
 	moviesMigrationPercent, err := strconv.Atoi(getEnv("MOVIES_MIGRATION_PERCENT", "50"))
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
 
-	eventsServiceUrl, err := url.Parse(getEnv("EVENTS_SERVICE_URL", "http://events-service:8082"))
-	if err != nil {
-		return nil, err
+	http.Handle("/", monolithHandler(monolithUrl))
+	http.HandleFunc("/health", handleHealth)
+
+	if gradualMigration {
+		http.Handle("/api/movies", moviesHandler(monolithUrl, moviesServiceUrl, moviesMigrationPercent))
 	}
 
-	eventsMigrationPercent, err := strconv.Atoi(getEnv("EVENTS_MIGRATION_PERCENT", "50"))
-	if err != nil {
-		return nil, err
-	}
-
-	return &Config{
-		Port:                   port,
-		MonolithUrl:            monolithUrl,
-		GradualMigration:       gradualMigration,
-		MoviesServiceUrl:       moviesServiceUrl,
-		MoviesMigrationPercent: moviesMigrationPercent,
-		EventsServiceUrl:       eventsServiceUrl,
-		EventsMigrationPercent: eventsMigrationPercent,
-	}, nil
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func getEnv(key string, defaultValue string) string {
@@ -103,14 +52,23 @@ func getEnv(key string, defaultValue string) string {
 	return value
 }
 
-func newWeightedReverseProxy(monolithUrl *url.URL, serviceUrl *url.URL, migrationPercent int) *httputil.ReverseProxy {
+func monolithHandler(monolithUrl *url.URL) *httputil.ReverseProxy {
 	return &httputil.ReverseProxy{
 		Rewrite: func(r *httputil.ProxyRequest) {
 			r.SetXForwarded()
-			if rand.IntN(100) >= migrationPercent {
+			r.SetURL(monolithUrl)
+		},
+	}
+}
+
+func moviesHandler(monolithUrl *url.URL, moviesServiceUrl *url.URL, moviesMigrationPercent int) *httputil.ReverseProxy {
+	return &httputil.ReverseProxy{
+		Rewrite: func(r *httputil.ProxyRequest) {
+			r.SetXForwarded()
+			if rand.IntN(100) >= moviesMigrationPercent {
 				r.SetURL(monolithUrl)
 			} else {
-				r.SetURL(serviceUrl)
+				r.SetURL(moviesServiceUrl)
 			}
 		},
 	}
